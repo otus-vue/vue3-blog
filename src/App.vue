ca668fc2c52497155dba77ff618176218dbc7a66
Ilya Nurullin <nurullinin@mail.ru>
2023-01-26 19:34:08 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Navigation></Navigation>
+  <Home></Home>
+</template>
+
+<script setup>
+import Navigation from "./views/general/Navigation.vue";
+import Home from "./views/Home.vue";
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <PageTemplate>
+    <template #header>
+      <PageHeader bg-image="/img/about-bg.jpg">
+        <template #header> About Me </template>
+        <template #subheader> This is what I do. </template>
+      </PageHeader>
+    </template>
+
+    <template #main>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+              nostrum ullam eveniet pariatur voluptates odit, fuga atque ea
+              nobis sit soluta odio, adipisci quas excepturi maxime quae totam
+              ducimus consectetur?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              praesentium recusandae illo eaque architecto error, repellendus
+              iusto reprehenderit, doloribus, minus sunt. Numquam at quae
+              voluptatum in officia voluptas voluptatibus, minus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+              consequuntur magnam, excepturi aliquid ex itaque esse est vero
+              natus quae optio aperiam soluta voluptatibus corporis atque iste
+              neque sit tempora!
+            </p>
+          </div>
+        </div>
+      </div>
+    </template>
+  </PageTemplate>
+</template>
+
+<script setup>
+import PageTemplate from "./general/PageTemplate.vue";
+import PageHeader from "./general/PageHeader.vue";
+</script>
